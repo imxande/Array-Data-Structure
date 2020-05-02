f0a6a083e5cf44d2e7d482fd5abc4cd04b7f172e
imxande <alerodriguezlarosa@gmail.com>
2020-05-02 16:26:07 -0400

    Arr push method inplemented

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ class Arr {
         this.length = 0;
         this.data = {}
     }
+    // add elements to the data structure
+    push(element){
+        // data at specific length holds the item
+        this.data[this.length] = element;
+        // I need to increase length size
+        this.length++;
+        // i need to return the data
+        return this.length;
+    }
 }
 
 const newArr = new Arr();
+newArr.push("a");
+newArr.push("b");
+newArr.push("c");
+newArr.push("d");
+newArr.push("e");
 
 console.log(newArr);
