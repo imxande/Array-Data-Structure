6312e3b80f6ebee7ece4465652243ebc3e6a8517
imxande <alerodriguezlarosa@gmail.com>
2020-05-02 16:30:28 -0400

    Arr get method implemented

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ class Arr {
         // i need to return the data
         return this.length;
     }
+    // access to specific element of the data structure
+    get(index){
+        return this.data[index];
+    }
 }
 
 const newArr = new Arr();
@@ -22,3 +26,5 @@ newArr.push("d");
 newArr.push("e");
 
 console.log(newArr);
+console.log(newArr.get(2));
+
